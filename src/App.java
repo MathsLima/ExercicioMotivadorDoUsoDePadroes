d4cd0b75a6ac05471aaf4a569917e12096bdf588
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2025-03-09 16:51:46 -0300

    exercicio 2

--- src/App.java
+++ src/App.java
@@ -1,27 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
+        System.out.println("\n");
+        System.out.println("-----------------------------------------------");
+
         Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
-        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
-
-        System.out.println("Tipos de veiculos:");
         System.out.println(basico);
-        System.out.println(esportivo);
-
-        System.out.println("\nAbastencendo carros");
-        System.out.println("Basico: " + basico.abastece(TipoCombustivel.GASOLINA, 55) + " litros.");
-        System.out.println("Esportivo: " + esportivo.abastece(TipoCombustivel.GASOLINA, 45) + " litros.");
-
-        System.out.println("\nViajando com o carros:");
-
-        basico.viaja(250);
+        System.out.println("\nAbastencendo carro basico com gasolina");
+        basico.abastece(TipoCombustivel.GASOLINA, 55);
         basico.viaja(150);
         System.out.println(basico);
-        System.out.println("\n");
 
-        esportivo.viaja(200);
-        esportivo.viaja(200);
+        System.out.println("-----------------------------------------------");
+
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA, 6, 45);
         System.out.println(esportivo);
-        System.out.println("\n");
+        System.out.println("\nAbastencendo carro esportivo com gasolina");
+        esportivo.abastece(TipoCombustivel.GASOLINA, 55);
+        esportivo.viaja(250);
+        System.out.println(esportivo);
+
+        System.out.println("-----------------------------------------------");
+
+        Carro utilitario = new Carro("Utilitario", TipoCombustivel.DIESEL, 5, 70);
+        System.out.println(utilitario);
+        System.out.println("\nAbastencendo carro utilitario com diesel");
+        utilitario.abastece(TipoCombustivel.DIESEL, 55);
+        utilitario.viaja(350);
+        System.out.println(utilitario);
     }
 }
